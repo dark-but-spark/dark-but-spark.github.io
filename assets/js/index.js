91e856bb9ca1589b182fba76dba0dda4a1697c98
chen jiyan <3036789610@qq.com>
2025-06-20 01:29:35 +0800

    初始界面 无美观优化

---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+
+const centerLeft = document.querySelector(".center_left");
+const centerRight = document.querySelector(".center_right");
+
+window.addEventListener("resize", function() {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    console.log("窗口大小已调整为:", newWidth, "x", newHeight);
+    let leftWidth = 0;
+    let rightWidth = 0;
+    if(newWidth < 800) 
+    {
+        rightWidth = newWidth;
+    }
+    else if(newWidth*0.618*0.25 < 800) 
+    {
+        leftWidth = 200;
+        rightWidth = 600;
+    }
+    else
+    {
+        leftWidth = newWidth * 0.618*0.25;
+        rightWidth = newWidth * 0.618*0.75;
+    }
+
+    centerLeft.style.width = leftWidth + "px";
+    centerRight.style.width = rightWidth + "px";
+    centerLeft.style.left=newWidth*0.5- (leftWidth+rightWidth)/2 + "px";
+    centerRight.style.right=newWidth*0.5- (leftWidth+rightWidth)/2 + "px";
+    if(leftWidth==0)
+    {
+        centerLeft.style.display = "none";
+    }else{
+        centerLeft.style.display = "block";
+    }
+    // console.log("左侧布局宽度:", leftWidth, "px");
+    // console.log("右侧布局宽度:", rightWidth, "px");
+    // console.log("左侧布局位置:", centerLeft.style.left);
+    // console.log("右侧布局位置:", centerRight.style.left);
+
+});
+
+// 获取当前时间
+const currentTime = new Date();
+console.log("当前时间:", currentTime.toLocaleString());
+
+
+
