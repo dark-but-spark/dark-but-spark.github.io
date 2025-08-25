83d7b73345e067c00f5d68831bc907ef33602189
chen jiyan <3036789610@qq.com>
2025-08-26 02:06:48 +0800

    Site updated: 2025-08-26 02:06:46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756142663862";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756145204416";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
